9cb4151fd26093783cd1b348f64b730807f46979
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2018-10-12 07:42:58 -0400

    Entendiendo authorization en aspnet core

---
+++ AspNetSecurity-NoSecurity/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetSecurity_NoSecurity.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetSecurity_NoSecurity
+{
+    public class Startup
+    {
+        private IHostingEnvironment _env;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            if (!_env.IsDevelopment())
+            {
+                services.Configure<MvcOptions>(options => 
+                    options.Filters.Add(new RequireHttpsAttribute()));
+            }
+
+            services.AddSingleton<ConferenceRepo>();
+            services.AddSingleton<ProposalRepo>();
+            services.AddSingleton<AttendeeRepo>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Conference}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
